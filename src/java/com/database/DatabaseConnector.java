3c5c791c6c58f235172a818d79deb0a9aab85b62
Omar Helwa <omar200582@icloud.com>
2024-11-04 10:45:56 +0200

    NormalPatient.java     1-Removed commented-out code.     2-Added Javadoc comments to the class and methods. Patient.java     1-Removed commented-out code.     2-Defined constants for repeated values like SQL queries.     3-Organized imports.     4-Used try-with-resources to ensure resources are closed properly.     5-Improved exception handling with more meaningful messages.     6-Added Javadoc comments to the class and methods. Person.java     1-Removed unnecessary default constructor.     2-Added Javadoc comments to the class and methods.     3-Removed unnecessary getID method. DatabaseConnector.java     1-Used constants for connection details.     2-Ensured proper resource management with try-with-resources.     3-Added Javadoc comments to the class and methods. Handler.java     1-Removed unnecessary DatabaseConnector.connect() calls.     2-Used try-with-resources to ensure resources are closed properly.     3-Added Javadoc comments to the class and methods.     4-Ensured consistent formatting and removed unnecessary semicolons.

--- src/java/com/database/DatabaseConnector.java
+++ src/java/com/database/DatabaseConnector.java
@@ -2,22 +2,33 @@ package com.database;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
-
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * Provides methods to connect to the database.
+ */
 public class DatabaseConnector {
 
+    private static final String CONN_STRING = "your_connection_string";
+    private static final String USERNAME = "your_username";
+    private static final String PASSWORD = "your_password";
 
     private static MysqlDataSource dataSource = new MysqlDataSource();
 
-    public static void connect() {
+    static {
         dataSource.setURL(CONN_STRING);
+        dataSource.setUser(USERNAME);
+        dataSource.setPassword(PASSWORD);
     }
 
-
-    public static Connection connection() throws SQLException {
-        return dataSource.getConnection(username, password);
+    /**
+     * Connects to the database and returns the connection.
+     *
+     * @return the database connection
+     * @throws SQLException if a database access error occurs
+     */
+    public static Connection connect() throws SQLException {
+        return dataSource.getConnection();
     }
-
 }
